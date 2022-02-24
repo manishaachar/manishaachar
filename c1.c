84b42045d386b949e8c9f4505dcfe27777b7533f
manishaachar <98947237+manishaachar@users.noreply.github.com>
2022-02-24 13:26:02 +0530

    CN

---
+++ c1.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int a[10][10],n;
+int i,j,k;
+int min(int a,int b)
+{
+return a<b?a:b;
+}
+void floyd()
+{
+int i,j,k;
+for(k=1;k<=n;k++)
+{
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+a[i][j]=min(a[i][j],a[i][k]+a[k][j]);
+}
+}
+}
+}
+
+int main()
+{
+int i,j;
+printf("Enter the no. of nodes");
+scanf("%d",&n);
+printf("Enter the matrix");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+{
+scanf("%d",&a[i][j]);
+}
+if(a[i][j]==0)
+a[i][j]=999;
+if(a[i][j])
+a[i][j]=0;
+
+floyd();
+printf("Distance of matrix is \n");
+
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+printf("%d\t",a[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
