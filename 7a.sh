5d95d8fb0fbdb65c7afa84ec221a8b676ff30f2a
manishaachar <98947237+manishaachar@users.noreply.github.com>
2022-02-25 14:20:14 +0530

    Update 7a.sh

--- 7a.sh
+++ 7a.sh
@@ -1,3 +1,6 @@
+#Write a shell script that gets executed and displays the message either “Good Morning” or
+#“Good Afternoon” or “Good Evening” depending upon time at which the user logs in.
+
 set -- `who`
 user=$1
 set -- `echo $4 |tr ":" " "`
