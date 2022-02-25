49504972efbc2015254d01b4a9fb3d88c265577b
manishaachar <98947237+manishaachar@users.noreply.github.com>
2022-02-25 14:12:02 +0530

    Update 1b.sh

--- 1b.sh
+++ 1b.sh
@@ -1,6 +1,6 @@
 #Write a shell script that accepts a path name and creates all the components in that path
-name as directories. For example, if the script is named as mpc, then the command mpc a/b/c/d
-should create sub-directories a, a/b, a/b/c, a/b/c/d.
+#name as directories. For example, if the script is named as mpc, then the command mpc a/b/c/d
+#should create sub-directories a, a/b, a/b/c, a/b/c/d.
 
 if [ $# -ne 0 ]
 then 
