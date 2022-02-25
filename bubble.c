09cfefb78767eda7ff6292df3a5daa3a34cc5100
manishaachar <98947237+manishaachar@users.noreply.github.com>
2022-02-25 14:38:44 +0530

    bubble sort

--- bubble.c
+++ bubble.c
@@ -1,3 +1,8 @@
+#Write a C program to implement the following sorting algorithms using user defined
+#functions: 
+#a. Bubble sort (Ascending order) 
+#b. Selection sort (Descending order).
+
 #include<stdio.h>
 int main()
 {
