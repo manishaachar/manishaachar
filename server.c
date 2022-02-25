9ee0ac835f727b3d8547cf8ff8449e9891229333
manishaachar <98947237+manishaachar@users.noreply.github.com>
2022-02-25 14:27:52 +0530

    Server

--- server.c
+++ server.c
@@ -1,3 +1,6 @@
+#Using TCP/IP sockets, write a client-server program to make the client send the file name
+#and to make the server send back the contents of the requested file if present.
+
 #include<stdio.h>
 #include<fcntl.h>
 #include<arpa/inet.h>
