7ea54b730c53b910ae84d1cd8c9e5bad5cff83ed
manishaachar <98947237+manishaachar@users.noreply.github.com>
2022-02-25 14:19:21 +0530

    Update 6b.sh

--- 6b.sh
+++ 6b.sh
@@ -1,3 +1,6 @@
+#Write a shell script to list all the files in a directory whose filename is at least 10 characters.
+#(use expr command to check the length).
+
 for i in `ls`
 do
 fl=`expr length $i`
